27c7beaf87527dafe8017bbd0c89f1fc9873ff33
Daniel Stahl <B0GX3@regions.com>
2018-08-03 14:55:02 -0500

    added different graph

--- src/Components/Graphs.js
+++ src/Components/Graphs.js
@@ -1,50 +1,37 @@
 import React from 'react'
-import { 
+import { LineChart, Line, ResponsiveContainer, XAxis} from 'recharts'
+/*import { 
     VictoryLine, 
     VictoryChart,
     VictoryAxis,
     VictoryLabel,
     VictoryScatter,
     VictoryLegend,
+    VictoryTooltip,
     VictoryVoronoiContainer
-} from 'victory'
+} from 'victory'*/
 import {connect} from 'react-redux'
 
 const domainPadding=25
-const splineLabelFn=label=>d=>`Transformed Option Price ${d.transformed_option} at ${label} ${d.log_strike}`
+//const splineLabelFn=label=>d=>`Transformed Option Price ${d.transformed_option} at ${label} ${d.log_strike}`
 
-const axisStyleOption={ axisLabel: { padding: 30} }
-const blueStroke={data:{stroke:"blue"}}
+//const axisStyleOption={ axisLabel: { padding: 30} }
+//const blueStroke={data:{stroke:"blue"}}
 
 const title='Spline of Option Prices'
 const xLabel='Normalized Log Strike'
 const yLabel='Transformed Option Price'
 const SplineCurves=({spline})=>(
-    spline.curve?<VictoryChart 
-        domainPadding={domainPadding} 
-        containerComponent={<VictoryVoronoiContainer labels={splineLabelFn(xLabel)}/>}
+    spline.curve?<ResponsiveContainer 
+        width={700}
+        height={'80%'}
     >
-       
-        <VictoryLabel x={120} y={50}
-            text={title}
-        />
-        <VictoryScatter data={spline.points} x="log_strike" y="transformed_option"/>
-        <VictoryLine
-            style={blueStroke}
-            interpolation="natural"
-            data={spline.curve}
-            x="log_strike"
-            y="transformed_option"
-        />
-        <VictoryAxis 
-            dependentAxis
-            style={axisStyleOption}
-            label={yLabel}
-        />
-        <VictoryAxis
-            label={xLabel}
-        />
-    </VictoryChart>:null
+        <LineChart data={spline.curve}>
+            <Line dataKey='transformed_option' type="monotone"/>
+            <XAxis dataKey='log_strike'/>
+        </LineChart>
+    </ResponsiveContainer>
+    :null
 )
 
 const mapStateToProps=({marketValues})=>({
--- src/Components/MarketValueTable.js
+++ src/Components/MarketValueTable.js
@@ -5,20 +5,20 @@ import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import {connect} from 'react-redux'
-
+import {FIXED_DECIMALS} from 'globals/constants'
 const MarketValueTable=({attributes})=>(
     attributes.asset?<Table>
         <TableHead>
             <TableRow>
                 <TableCell>Asset Price</TableCell>
                 <TableCell>Risk Free Rate</TableCell>
-                <TableCell>Time to Maturity</TableCell>
+                <TableCell>Years to Maturity</TableCell>
             </TableRow>
         </TableHead>
         <TableBody>
-            <TableCell>{attributes.asset}</TableCell>
-            <TableCell>{attributes.rate}</TableCell>
-            <TableCell>{attributes.maturity}</TableCell>
+            <TableCell>{attributes.asset.toFixed(FIXED_DECIMALS)}</TableCell>
+            <TableCell>{attributes.rate.toFixed(FIXED_DECIMALS)}</TableCell>
+            <TableCell>{attributes.maturity.toFixed(FIXED_DECIMALS)}</TableCell>
         </TableBody>
     </Table>:null
 )
