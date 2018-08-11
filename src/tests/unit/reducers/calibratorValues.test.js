5e0d1f7234a7f0102784d0318aa626c1aeba8c3b
Daniel Stahl <B0GX3@regions.com>
2018-08-11 09:28:00 -0500

    added test for calibratorValues

--- src/tests/unit/reducers/calibratorValues.test.js
+++ src/tests/unit/reducers/calibratorValues.test.js
@@ -1,7 +1,63 @@
 import {attributes} from 'Reducers/calibratorValues'
+import {
+    MARKET_VALUES,
+    CONSTRAINTS,
+    TICKER_VALUE
+} from 'Actions/constants'
 
 describe('attributes', ()=>{
     it('returns default attributes when called with no matching type', ()=>{
         expect(attributes(undefined, {type:'not match'})).toEqual({num_u:8, quantile:.01, prices:[], strikes:[]})
+    })
+    it('returns attributes and default when called with market values', ()=>{
+        expect(attributes(
+            undefined, 
+            {
+                type:MARKET_VALUES,
+                attributes:{hello:'world'}
+            })
+        ).toEqual({
+            num_u:8, quantile:.01, 
+            prices:[], strikes:[], 
+            hello:'world'
+        })
+    })
+    it('returns constraints and default when called with constraints', ()=>{
+        expect(attributes(
+            undefined, 
+            {
+                type:CONSTRAINTS,
+                constraints:{rho:.3}
+            })
+        ).toEqual({
+            num_u:8, quantile:.01, 
+            prices:[], strikes:[], 
+            constraints:{
+                rho:.3,
+                lambda:undefined,
+                mu_l:undefined,
+                sig_l:undefined,
+                v0:undefined,
+                speed:undefined,
+                eta_v:undefined
+            }
+        })
+    })
+    it('returns default attributes when ticker called and additional attributes exist', ()=>{
+        const attr={hello:'world'}
+        const state=attributes(
+            undefined, 
+            {
+                type:MARKET_VALUES,
+                attributes:attr
+            }
+        )
+        expect(attributes(state, {
+            type:TICKER_VALUE
+        })).toEqual({
+            constraints:undefined, 
+            num_u:8, quantile:.01, 
+            prices:[], strikes:[]
+        })
     })
 })
