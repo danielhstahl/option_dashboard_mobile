4b7557eac3a68b5ad7790cf887c8e6d9434d9ddc
Daniel Stahl <B0GX3@regions.com>
2018-08-11 09:35:42 -0500

    added service tests

---
+++ src/tests/unit/Services/inputHandler.test.js
@@ -0,0 +1,13 @@
+import {handleIV} from 'Services/inputHandler'
+
+describe('handleIV', ()=>{
+    it('returns false if array has length zero', ()=>{
+        expect(handleIV([])).toEqual(false)
+    })
+    it('returns true if array has length greater than zero and key iv', ()=>{
+        expect(handleIV([{iv:'hello'}])).toEqual(true)
+    })
+    it('returns false if array has length greater than zero and does not have key iv', ()=>{
+        expect(handleIV([{hello:'hello'}])).toEqual(false)
+    })
+})
